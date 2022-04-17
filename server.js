3551873608267bd05d184e4c4741fe1de69aa307
zhang <2546639045@qq.com>
2022-04-17 21:05:36 +0800

    修改版

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    response.write(`你来了啊`)
     response.end()
   } else if(path === '/x'){
     response.statusCode = 200
