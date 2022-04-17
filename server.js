71c15300b31faaf3ad90b6be26467f32bbcca420
zhang <2546639045@qq.com>
2022-04-17 21:22:42 +0800

    修改版

--- server.js
+++ server.js
@@ -24,12 +24,20 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你来了啊`)
+    response.write(`
+        <!DOCTYPE html>
+        <head>
+            <link rel="stylesheet" href="/style.css">
+        </head>
+        <body>
+            <h1>你来了啊</h1>
+        </body>
+    `)
     response.end()
-  } else if(path === '/x'){
+  } else if(path === '/style.css'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`body{color: red;}`)
+    response.write(`h1{color: red;}`)
     response.end()
   } else {
     response.statusCode = 404
